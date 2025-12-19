a9e2dfee5c97c632827d49c3c2d2ee4744a16580
halcyonoidd <agriazzukhruf1@gmail.com>
2025-12-19 09:46:43 +0700

    update aboutSection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,20 @@
 
 
 <section>
+
     <hr>
+
     <div id="mainSection" class="bg-[url('/img/milesMorales.gif')] bg-cover bg-center min-h-screen flex flex-col bg-blend-overlay bg-black/80">
         <main class="flex-grow flex items-center justify-center">
             <div class="text-left text-white px-4 mr-25">
                 <h1 class="text-5xl font-bold mb-2 text-left">Agri Azzukhruf</h1>
-                <p class="text-lg text-gray-400 mb-7">Surabaya, Indonesia</p>
-                <p class="text-lg mb-8 max-w-2xl mx-auto text-left">
-                    Passionate about crafting intuitive and dynamic web experiences. I focus on building responsive, high-performance websites that bring creative ideas to life.
-                </p>
-                
-                <div class="flex justify-end mb-8">
-                    <a href="https://github.com/halcyonoidd" target="_blank" rel="noopener noreferrer" class="group">
-                        <img src="/img/github2.png" alt="GitHub" class="w-15 h-15 transition-transform duration-300 group-hover:scale-110 group-hover:shadow-lg "/>
-                    </a>
-
-                    <a href="https://www.linkedin.com/in/agri-azzukhruf/" target="_blank" rel="noopener noreferrer" class="group">
-                        <img src="/img/linkedin.png" alt="LinkedIn" class="w-15 h-15 transition-transform duration-300 group-hover:scale-110 group-hover:shadow-lg "/>
-                    </a>
-                </div>
-
-                <div class="flex justify-between items-center">
-                    <div>
-                        <a href="#dataSection" class="ml-4 bg-transparent border-2 border-red-500 hover:bg-red-500 hover:text-white text-white font-semibold py-3 px-6 rounded-lg transition duration-300">Get To Know Me</a>
-                        <a href="/project" class="ml-4 bg-transparent border-2 border-white hover:bg-white hover:text-black text-white font-semibold py-3 px-6 rounded-lg transition duration-300">My Projects</a>
-                    </div>
+                    <p class="text-lg text-gray-400 mb-7">Surabaya, Indonesia</p>
+                    <p class="text-lg mb-8 max-w-2xl mx-auto text-left">
+                        Passionate about crafting intuitive and dynamic web experiences. I focus on building responsive, high-performance websites that bring creative ideas to life.
+                    </p>
                     
-                    <a download="cv-en.pdf" href="/assets/cv/cv-en.pdf" class="text-white underline hover:text-gray-300 transition duration-300 inline-flex items-center">
+                <div class="flex justify-start mb-8">
+                    <a download="cv-en.pdf" href="/assets/cv/cv-en.pdf" class="text-white underline hover:text-gray-300 transition duration-300 inline-flex ">
                         <svg class="w-5 h-5 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path>
                         </svg>
@@ -35,64 +22,76 @@
                     </a>
                 </div>
 
-            </div>
 
-
-
-            <div class="bg-[url('/img/fotoAgri2.jpg')] bg-cover bg-center bg-black/60 hidden md:block w-1/3 h-96 rounded-lg mr-10">
-            </div>
-        </main>
-
-
-    </div>
-    <hr/>
-    <br>
-
-    <div id="dataSection" class="py-16">
-        <div class="max-w-7xl mx-auto px-4">
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
-                <div class="space-y-6">
-                    <h2 class="text-4xl font-bold mb-8">WHO AM I ?</h2>
-                    <div class="space-y-4 text-sm md:text-lg leading-relaxed font-light">
-                        <p>
-                            I Am an undergraduate Computer Science student at Universitas Negeri Surabaya.
-                        </p>
-                        <p>
-                            Programming is my passion, that's the reason I am working at the time of my academic career.
-                        </p>
-                        <p>
-                            Additionally, I like to play games and watch movies/series in my leisure time which is kinda fun and relaxing!
-                        </p>
+                <div class="flex justify-between items-center">
+                    <div>
+                        <a href="#dataSection" class="ml-4 bg-transparent border-2 border-red-500 hover:bg-red-500 hover:text-white text-white font-semibold py-3 px-6 rounded-lg transition duration-300">Get To Know Me</a>
+                        <a href="/project" class="ml-4 bg-transparent border-2 border-white hover:bg-white hover:text-black text-white font-semibold py-3 px-6 rounded-lg transition duration-300">My Projects</a>
                     </div>
-                    <div class="pt-4">
-                        <a download="cv-en.pdf" href="/assets/cv/cv-en.pdf" class=" bg-red-500 hover:bg-red-700 text-white font-semibold py-3 px-6 rounded-lg transition duration-300">
-                            Download CV
+
+                    <div class="flex justify-end">
+                        <a href="https://github.com/halcyonoidd" target="_blank" rel="noopener noreferrer" class="group">
+                            <img src="/img/github2.png" alt="GitHub" class="w-15 h-15 transition-transform duration-300 group-hover:scale-110 group-hover:shadow-lg "/>
+                        </a>
+
+                        <a href="https://www.linkedin.com/in/agriazzukhruf" target="_blank" rel="noopener noreferrer" class="group">
+                            <img src="/img/linkedin.png" alt="LinkedIn" class="w-15 h-15 transition-transform duration-300 group-hover:scale-110 group-hover:shadow-lg "/>
                         </a>
                     </div>
                 </div>
+            </div>
 
-                <div class="relative h-96 md:h-full flex items-center justify-center">
-                    <div class="absolute top-[5%] left-[5%] transform -rotate-12 hover:scale-110 transition duration-500 z-10">
-                        <div class="absolute inset-0 bg-blue-500 blur-[40px] opacity-40 rounded-full"></div>
-                        <img src="/img/fotoAgri2.jpg" 
-                             alt="Profile 1" 
-                             class="relative w-24 h-32 md:w-32 md:h-44 object-cover rounded-xl border-2 border-white/10 shadow-2xl">
-                    </div>
+            <div class="bg-[url('/img/fotoAgri2.jpg')] bg-cover bg-center bg-black/60 hidden md:block w-1/3 h-96 rounded-lg mr-10"></div>
+        </main>
+    </div>
 
-                    <div class="absolute bottom-[5%] left-[25%] transform rotate-6 hover:scale-110 transition duration-500 z-20">
-                        <div class="absolute inset-0 bg-gray-100 blur-[50px] opacity-20 rounded-full"></div>
+    <hr/>
+    <br>
+
+    <div id="dataSection" class="py-16 relative overflow-hidden">
+        <div class="max-w-7xl mx-auto px-4">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
+                <div class="relative h-96 max-w-md mx-auto md:mx-0">
+                    <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rotate-2 z-20 
+                                hover:rotate-0 hover:scale-105 transition-all duration-500">
+                        <div class="absolute inset-0 rounded-3xl"></div>
                         <img src="/img/fotoAgri.jpg" 
-                             alt="Profile 2" 
-                             class="relative w-32 h-44 md:w-48 md:h-64 object-cover rounded-2xl border-2 border-white/10 shadow-2xl">
+                             alt="Agri Azzukhruf" 
+                             class="relative w-48 h-64 object-cover rounded-2xl shadow-2xl border-4 border-white/10">
+                    </div>
+                    
+                    <div class="absolute top-8 right-4 rotate-6 z-10
+                                hover:rotate-3 hover:scale-105 transition-all duration-500">
+                        <div class="absolute inset-0 rounded-3xl"></div>
+                        <img src="/img/fotoAgri5.jpeg" 
+                             alt="Agri Azzukhruf" 
+                             class="relative w-32 h-40 object-cover rounded-xl shadow-2xl border-4 border-white/10">
                     </div>
 
-                    <div class="absolute top-[10%] right-[0%] transform -rotate-6 hover:scale-110 transition duration-500 z-10">
-                        <div class="absolute inset-0 bg-orange-400 blur-[50px] opacity-30 rounded-full"></div>
+                    <div class="absolute bottom-4 left-8 -rotate-6 z-10
+                                hover:-rotate-3 hover:scale-105 transition-all duration-500">
+                        <div class="absolute inset-0 rounded-3xl"></div>
                         <img src="/img/fotoAgri3.jpg" 
-                             alt="Profile 3" 
-                             class="relative w-32 h-40 md:w-44 md:h-56 object-cover rounded-xl border-2 border-white/10 shadow-2xl">
+                             alt="Agri Azzukhruf" 
+                             class="relative w-36 h-44 object-cover rounded-xl shadow-2xl border-4 border-white/10">
                     </div>
                 </div>
+                
+                <div class="space-y-6">
+                    <h2 class="text-4xl font-bold mb-8">WHO AM I ?</h2>
+                    
+                    <div class="space-y-4 text-sm md:text-lg leading-relaxed font-light text-gray-400 mb-10">
+                        <p>I'm an undergraduate Computer Science student at Universitas Negeri Surabaya.</p>
+                        <p>Programming is my passion, that's the reason I am working at the time of my academic career.</p>
+                        <p>I like to play games and watch movies/series in my leisure time which is kinda fun and relaxing!</p>
+                        <p>Additionaly, I also like to hike and explore nature.</p>
+                    </div>
+                    
+                    <div>
+                        <a href="/about" class="bg-transparent border-2 border-red-500 hover:bg-red-500 hover:text-white text-white font-semibold py-3 px-6 rounded-lg transition duration-300">Get To Know Me</a>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
